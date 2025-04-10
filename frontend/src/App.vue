d78071a829db992dfe329eb8d34ad677b8902eba
ypAnalysis <yapa02219@t-online.de>
2025-04-10 11:40:07 +0200

    Add .env, fix process.env.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -36,13 +36,3 @@ const appName = process.env.VUE_APP_NAME;
 
 const analysisView = computed(() => route.name === "AnalysisView");
 </script>
-
-<style>
-#sportVid {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-}
-</style>
