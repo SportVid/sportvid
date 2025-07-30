8820b2c7cd86beb811a28c31920357ebc2a9b44f
alex k <alexander.krawczyk@cs.hs-fulda.de>
2025-07-30 15:51:52 +0200

    implemented bbox edit

--- backend/src/backend/backend/views/bounding_boxes.py
+++ backend/src/backend/backend/views/bounding_boxes.py
@@ -2,13 +2,14 @@ import json
 import logging
 from functools import wraps
 
+from django.db import transaction
 from django.views import View
 from django.http import JsonResponse
 
 from backend.models import PluginRunResult
 from backend.utils.decode_auth import decode_and_authenticate
 
-from data import DataManager
+from data import DataManager, BboxesData, BboxData
 
 logger = logging.getLogger(__name__)
 
@@ -17,16 +18,12 @@ class BoundingBoxesChange(View):
     @decode_and_authenticate(require_name=False)
     def post(self, request, data):
         try:
-            logging.error(data)
             bytetrack_result_id = data.get("bytetrack_run_id")
-            bytetrack_prr_db = PluginRunResult.objects.get(
-                id=bytetrack_result_id
-            )
             current_ref_id = None
-            if "ref_id" in data: current_ref_id = data.get("ref_id")
+            if "ref_id" in data: current_ref_id = int(data.get("ref_id"))
         
             new_ref_id = None; new_team_id = None
-            if "new_ref_id" in data: new_ref_id = data.get("new_ref_id")  
+            if "new_ref_id" in data: new_ref_id = int(data.get("new_ref_id"))
             if "new_team_id" in data: new_team_id = data.get("new_team_id")
             
             if current_ref_id is None or (new_ref_id is None and new_team_id is None):
@@ -38,25 +35,36 @@ class BoundingBoxesChange(View):
                     update_all_ref_id = True
             if "update_all_team_id" in data:
                 if data.get("update_all_team_id") in ['true', 'True']:
-                    update_all_team_id = True     
-            
-            logging.error(f'{current_ref_id},{new_ref_id},{new_team_id},{update_all_ref_id},{update_all_team_id}')    
+                    update_all_team_id = True   
+  
+            with transaction.atomic():
+                bytetrack_prr_db = PluginRunResult.objects.get_or_create(
+                    plugin_run_id=bytetrack_result_id
+                )
+                logging.error(bytetrack_prr_db[0].data_id)
+                logging.error(f'{current_ref_id},{new_ref_id},{new_team_id},{update_all_ref_id},{update_all_team_id}')    
 
-            manager = DataManager("/predictions/")     
-            # TODO: what would be the most efficient way to update the old data 
-            with manager.load(bytetrack_prr_db.data_id) as bboxes_data:
-                # NOTE: returns bboxes_data as BboxesData List-Wrapper
-                bbd_dict = bboxes_data.to_dict()
-                for entry in bbd_dict:
-                    if entry.get("ref_id") == current_ref_id:
-                        logging.error(entry.get("ref_id"))
+                manager = DataManager("/predictions/")
+                # manipulate unzipped files and create an altered 'BBoxesData' object
+                with manager.load(bytetrack_prr_db[0].data_id) as bboxes_data:
+                    bbd = bboxes_data.to_dict()
+                    bbd_data = bbd["bboxes"]
+                    with manager.create_data("BboxesData") as altered_bbx:
+                        for entry in bbd_data:
+                            if entry.get("ref_id") == current_ref_id:
+                                entry["ref_id"] = new_ref_id
+                            bbox = BboxData(**entry)
+                            altered_bbx.bboxes.append(bbox)
+                logging.error(altered_bbx.id)
                 
-                logging.error(bboxes_data.to_dict())
-                # access to unziped files
-            
-            # with data_manager.create_data(data_type):
-            
-            # data_manager.delete -> physical delete 
+                # remove old data from the fs
+                manager.delete(bytetrack_prr_db.data_id)
+                # change "data_id" reference to the id of the altered bbox data
+                bytetrack_prr_db.update({"data_id": altered_bbx.id})
+                
+                logging.error(bytetrack_prr_db.data_id)
+
+            return JsonResponse({"status": "ok", "entry": altered_bbx.to_dict()})
         except Exception:
             logger.exception('Failed to edit bounding box(es)')
             return JsonResponse({"status": "error"})
