fix ArchiveFileWrapper bug, create KPI computation in plugin

inference_ray/src/inference_ray/plugins/floodlight_convert.py
@@ -63,6 +63,11 @@ class FloodlightConvert(
             # read_pitch_from_mat_info_xml, 
             read_position_data_xml
         )
+
+        from floodlight.models.geometry import CentroidModel
+        from floodlight.models.kinematics import DistanceModel, VelocityModel
+        from floodlight.models.kinetics import MetabolicPowerModel
+
         if "format" not in parameters:
             raise ValueError("'format' is required for plugin execution.")
         
@@ -108,12 +113,30 @@ class FloodlightConvert(
         # TODO: KPI computation
         xy_pos = sampled_data
         meta_data = {"some_meta_data": 1337}
+        DistMod = DistanceModel()
+        VelMod = VelocityModel()
+        MetPowMod = MetabolicPowerModel()
+        CentMod = CentroidModel()
+
+        kpi_dict = {}
+        for half in ["firstHalf", "secondHalf"]:
+            for team in ["Home", "Away"]:
+                DistMod.fit(xy[half][team])
+                VelMod.fit(xy[half][team])
+                MetPowMod.fit(xy[half][team])
+                CentMod.fit(xy[half][team])
+
+            kpi_dict[f'distance_covered_{half}_{team}'] = np.array(DistMod.cumulative_distance_covered())
+            kpi_dict[f'max_velocity_{half}_{team}'] = np.nanmax(VelMod.velocity(), axis=0).round(2)
+            kpi_dict[f'metabolic_power_{half}_{team}'] = np.array(MetPowMod.metabolic_power())
+            kpi_dict[f'centroid_{half}_{team}'] = CentMod.centroid().xy
         # ----------------- OUTPUT
         # TODO: specify output type based on what you need, see "packages/data/src/data/plugins/tracking_data.py"
         with data_manager.create_data("FloodlightData") as fl_data:
             fl_data.name = "fl_data"
             fl_data.tracking_data_id = parameters.get('tracking_data_id') 
-            fl_data.meta_data = json.dumps(meta_data)
+            #fl_data.meta_data = json.dumps(meta_data)
+            fl_data.kpis = kpi_dict
             fl_data.xy_pos = xy_pos
             
             self.update_callbacks(callbacks, progress=1.0)