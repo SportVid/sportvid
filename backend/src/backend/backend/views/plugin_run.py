bf0c7c1b7c135494087443e41376aba4f2757773
ypAnalysis <yapa02219@t-online.de>
2026-02-27 14:39:50 +0100

    Fix PluginDelete.

--- backend/src/backend/backend/views/plugin_run.py
+++ backend/src/backend/backend/views/plugin_run.py
@@ -161,14 +161,15 @@ class PluginRunDelete(View):
                     {"status": "error", "type": "missing_values_plugin_list"}
                 )
 
-            if list(data.get("plugin_list"))[0] == 'all':
-                response, _ = PluginRun.objects.all().delete()
+            plugin_list = data.get("plugin_list")
+
+            if list(plugin_list)[0] == 'all':
+                response, _ = PluginRun.objects.filter(video__owner=request.user).delete()
             else:
-                plugins_to_be_deleted = [
-                    PluginRun.objects.get(id=p) for p in PluginRun.objects.filter(video__owner=request.user).values_list('id', flat=True)
-                ]
-                for p in plugins_to_be_deleted:
-                    response = p.delete()
+                response, _ = PluginRun.objects.filter(
+                    id__in=plugin_list,
+                    video__owner=request.user,
+                ).delete()
 
             return JsonResponse({"status": "ok", "deleted_items": response})
         except Exception:
