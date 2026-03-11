8ccdd17b8e47fde6fc69287830885c2e5adff1e7
fdai0234 <alexander.krawczyk@informatik.hs-fulda.de>
2026-03-11 18:09:07 +0100

    fix manage.sh

--- manage.sh
+++ manage.sh
@@ -49,11 +49,6 @@ prepare() {
     original_dir=$(pwd)
 
     echo "Executing environment '$ENVIRONMENT' on branch '$BRANCH'"
-prepare() {
-    local original_dir
-    original_dir=$(pwd)
-
-    echo "Executing environment '$ENVIRONMENT' on branch '$BRANCH'"
     
     if ! cd /git/sportvid; then
         echo "Failed to change directory to /git/sportvid"
@@ -101,8 +96,6 @@ exec_docker(){
 }
 
 case $COMMAND in
-
-
     "build")
         DOCKER_CMD="up --build -d"
         DOCKER_CMD="up --build -d"
@@ -156,17 +149,6 @@ case $COMMAND in
             exec_docker
         fi
         ;;
-    # NOTE: obsolete since frontend container executes 'npm run build' and copies the file into the shared volume.
-    # "frontend-install")
-    #     DOCKER_CMD="exec frontend npm install"
-    #     echo "Installing npm packages..."
-    #     exec_docker
-	#     ;;
-    # "frontend-build")
-	#     DOCKER_CMD="exec frontend npm run build"
-    #     echo "Building the frontend..."
-	#     exec_docker
-    #     ;;
     # NOTE: obsolete since frontend container executes 'npm run build' and copies the file into the shared volume.
     # "frontend-install")
     #     DOCKER_CMD="exec frontend npm install"
