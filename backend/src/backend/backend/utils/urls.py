c9b116f55aea918c3d66e04ee231eb7acda6ccce
fdai0234 <alexander.krawczyk@informatik.hs-fulda.de>
2026-06-22 20:36:36 +0200

    ...

--- backend/src/backend/backend/utils/urls.py
+++ backend/src/backend/backend/utils/urls.py
@@ -7,9 +7,12 @@ from django.conf import settings
 def media_url_to_file(id, ext):
     return "https://sportvid.dshs-koeln.de" + settings.MEDIA_URL + id[0:2] + "/" + id[2:4] + "/" + id + ext
 
+# TODO: fix MEDIA_ROOT path bug.
 def media_path_to_file(id, ext):
-    logging.error(settings.MEDIA_ROOT)
-    return os.path.join(settings.MEDIA_ROOT, id[0:2], id[2:4], f"{id}{ext}")
+    logging.error(f'settings.MEDIA_ROOT={settings.MEDIA_ROOT}')
+    return os.path.join("/media/", id[0:2], id[2:4], f"{id}{ext}")
 
+# TODO fix MEDIA_ROOT path bug.
 def media_dir_to_file(id):
-    return os.path.join(settings.MEDIA_ROOT, id[0:2], id[2:4])
+    logging.error(f'settings.MEDIA_ROOT={settings.MEDIA_ROOT}')
+    return os.path.join("/media/", id[0:2], id[2:4])
