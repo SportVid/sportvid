2fc81834bcda20d339f1e9ed69304670a29dd359
fdai0234 <alexander.krawczyk@informatik.hs-fulda.de>
2026-06-22 21:35:40 +0200

    fix.

--- backend/src/backend/backend/utils/urls.py
+++ backend/src/backend/backend/utils/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 def media_url_to_file(id, ext):
-    return "https://dev.sportvid.dshs-koeln.de" + settings.MEDIA_URL + id[0:2] + "/" + id[2:4] + "/" + id + ext
+    return "https://sportvid.dshs-koeln.de" + settings.MEDIA_URL + id[0:2] + "/" + id[2:4] + "/" + id + ext
 
 def media_path_to_file(id, ext):
     return settings.MEDIA_ROOT + id[0:2] + "/" + id[2:4] + "/" + id + ext
